89eeebce584cfb59ce9c5f9c7f3041fd9f57b85d
Alex Tejada <tejada72@students.rowan.edu>
2018-12-10 18:31:53 -0500

    Driver update

--- Driver.java
+++ Driver.java
@@ -2,11 +2,13 @@ public class Driver {
     public static void main(String[] args){
 
         //Initialize Operating system
-        OperatingSystem OS = OperatingSystem.getInstance();
-        OS.setScheduler(new FakeScheduler());
-        OS.setMemoryManager(new MemoryManager(new FirstFit(500)));
+        OperatingSystem os = new OperatingSystem();
+        os.setScheduler(new FakeScheduler(new ProcessList()));
+        os.setMemoryManager(new MemoryManager(new WorstFit(1024)));
+        View view = new View();
+        os.addObserver(view);
 
         //Start running the Operating system
-        OS.run();
+        os.run();
     }
 }
